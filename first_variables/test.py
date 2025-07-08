73d0d7ff6c9f3d0ee5d588d910542a5e58a745eb
ravi2krishna <ravi2krishna@gmail.com>
2025-07-08 09:29:20 +0530

    problem with concat

--- first_variables/test.py
+++ first_variables/test.py
@@ -1,5 +1,4 @@
-2name = "Ravi" # not valid, starts with a digit
-_name = "Ravi" # valid
-class = "Ravi" # not valid, class is a keyword
-first-name = "Ravi" # not valid, only _ allowed
-
+# define variables
+name = "ravi"
+session = 9
+print("My name is "+name+ " session is at " +session)
