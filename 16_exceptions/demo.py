28fce171b1adfcaa73d829f93dc326993f65ab9b
ravi2krishna <ravi2krishna@gmail.com>
2025-08-22 09:55:03 +0530

    Custom Exception Handling

--- 16_exceptions/demo.py
+++ 16_exceptions/demo.py
@@ -39,3 +39,90 @@ except:
     print("OOPS! You Cannot Divide By Zero - https://en.wikipedia.org/wiki/Division_by_zero")
     
 print("Program Execution Completed")
+print("="*50)
+
+# Multiple Types Of Exceptions
+list_data = [1,2,'Python',0,5,6]
+for i in list_data:
+    try:
+        print(1/i)
+    except:
+        print("OOPS! Something went wrong")
+    # TypeError: unsupported operand type(s) for /: 'int' and 'str'
+print("Program Execution Completed")
+print("="*50)    
+
+# Multiple Types Of Exceptions
+import sys
+list_data = [1,2,'Python',0,5,6]
+for i in list_data:
+    try:
+        print(1/i)
+    except:
+        print(sys.exc_info())
+        print("OOPS! Something went wrong")
+    # TypeError: unsupported operand type(s) for /: 'int' and 'str'
+print("Program Execution Completed")
+print("="*50) 
+
+# Multiple Types Of Exceptions
+import sys
+list_data = [1,2,'Python',0,5,6]
+for i in list_data:
+    try:
+        print(1/i)
+    except TypeError:
+        print("OOPS! Don't divide Strings with numbers")
+    except ZeroDivisionError:
+        print("OOPS! Don't divide numbers with zero")
+print("Program Execution Completed")
+print("="*50)  
+
+# else block -> runs if no exception
+print("="*50)
+print("Program Execution Started")
+num1 = 10
+num2 = 5
+try:
+    print(num1/num2) 
+except:
+    print("OOPS! You Cannot Divide By Zero - https://en.wikipedia.org/wiki/Division_by_zero")
+else:
+    print("Calculation Was Successful!!! proceeding with further steps")
+finally:
+    print("Program Execution Completed")
+print("="*50)
+
+# User Defined Exceptions
+class AgeTooYoungError(Exception):
+    pass
+
+class NoIDError(Exception):
+    pass
+
+# age = int(input("Enter Your Age: "))
+# if age < 18:
+#     raise AgeTooYoungError("You must be at least 18 years old to register")
+# else:
+#     has_id = input("Do you have ID ? (yes/no) ")
+#     if has_id != "yes":
+#         raise NoIDError("You must have an ID to register") 
+# print("Registration Successful!")
+# print("="*50)
+
+try:
+    age = int(input("Enter Your Age: "))
+    if age < 18:
+        raise AgeTooYoungError
+    has_id = input("Do you have ID ? (yes/no) ")
+    if has_id != "yes":
+        raise NoIDError
+except AgeTooYoungError:
+    print("You must be at least 18 years old to register")
+
+except NoIDError:
+    print("You must have an ID to register") 
+else:        
+    print("Registration Successful!")
+finally:
+    print("Exiting Application!")    
