be55c78060a1bc59e0d8f8f3f36ec6997ab78d05
ravi2krishna <ravi2krishna@gmail.com>
2025-08-22 10:28:12 +0530

    OOPS Basics

---
+++ 17_oops_basics/demo.py
@@ -0,0 +1,13 @@
+# class
+class Laptop:
+    # data -> attributes / variables 
+    brand = "Apple"
+    screen = 13
+    
+    # method
+    def play_video():
+        print(f"Playing Video In 4k Content")
+
+# object
+mac = Laptop()
+
